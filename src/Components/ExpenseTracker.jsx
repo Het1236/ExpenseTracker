f0d5d255b8fa9a40c7285eba9fbc1aabae58732a
Het1236 <1236hetpatel@gmail.com>
2025-07-02 09:46:17 +0530

    css

--- src/Components/ExpenseTracker.jsx
+++ src/Components/ExpenseTracker.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './ExpenseTracker.css'; // Link to external dark-theme CSS
+import 'src/Components/ExpenseTracker.css'; // Link to external dark-theme CSS
 
 function ExpenseTracker() {
   const [totalBalance, setTotalBalance] = useState(0);
